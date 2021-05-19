85b4d1c98f6bb6cb7bb6da36d6119ef93e98ceeb
Shakti Rathore <rathoreshakti7@gmail.com>
2021-05-19 10:06:23 +0530

    removing dynamo table

--- nested/test2/test3/backend.tf
+++ nested/test2/test3/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     region = "ap-south-1"
     bucket = "internal.shuttl.tfstate"
-    dynamodb_table = "terraform-locks"
     profile = "tools"
     key = "pod/mumbai/shakti2021/test.tfstate"
   }
